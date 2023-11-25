6cb74bd9189508622fda976c419748a009a1d801
Sydney Penix <sydneyrpenix@students.abtech.edu>
2023-11-24 22:15:20 -0500

    Phase05 complete

--- phase05/private/db_credentials.php
+++ phase05/private/db_credentials.php
@@ -1,12 +1,12 @@
 <?php
-// local
-define("DB_SERVER", "localhost");
-define("DB_USER", "spenix");
-define("DB_PASS", "Deadmau5fan1997!");
-define("DB_NAME", "salamanders");
-
-//a2
+// // local
 // define("DB_SERVER", "localhost");
-// define("DB_USER", "charli12_doall");
-// define("DB_PASS", "ErYje7HV2o2Lqp7B9gx");
-// define("DB_NAME", "charli12_salamanders");
+// define("DB_USER", "spenix");
+// define("DB_PASS", "Deadmau5fan1997!");
+// define("DB_NAME", "salamanders");
+
+// a2
+define("DB_SERVER", "localhost");
+define("DB_USER", "sydneyrp_admin");
+define("DB_PASS", "Deadmau5fan1997!");
+define("DB_NAME", "sydneyrp_salamanders");
